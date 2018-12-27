6412c4f61db66a4206a0f070bdd39a15d86bd417
leileizhang <3448771943@qq.com>
2018-12-27 20:51:39 +0800

    update

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const createHandler = require("github-webhook-handler");
+const handler = createHandler({ path: "/webhook", secret: "bookinfo-node" });
+
+http
+  .createServer(function(req, res) {
+    handler(req, res, function(err) {
+      res.statusCode = 404;
+      res.end("no such location");
+    });
+  })
+  .listen(7777);
+
+handler.on("error", function(err) {
+  console.error("Error:", err.message);
+});
+
+handler.on("push", function(event) {
+  console.log(
+    "Received a push event for %s to %s",
+    event.payload.repository.name,
+    event.payload.ref
+  );
+});
