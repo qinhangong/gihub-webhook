9c8ceff69d0d17918426d6edfead0279117ffa12
leileizhang <3448771943@qq.com>
2019-01-11 11:17:41 +0800

    update

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const http = require("http");
-const spawn = require('child_process').spawn;
+const spawn = require("child_process").spawn;
 const createHandler = require("github-webhook-handler");
 const handler = createHandler({ path: "/webhook", secret: "bookinfo-node" });
 
 const rumCommand = (cmd, args, callback) => {
-  const child = spawn(cmd, args)
-  let response = ''
-  child.stdout.on('data', buffer => response += buffer.toString())
-  child.stdout.on('end', () => callback(response))
-}
+  const child = spawn(cmd, args);
+  let response = "";
+  child.stdout.on("data", buffer => (response += buffer.toString()));
+  child.stdout.on("end", () => callback(response));
+};
 
 http
   .createServer(function(req, res) {
+    console.log(123456);
     handler(req, res, function(err) {
       res.statusCode = 404;
       res.end("no such location");
     });
   })
-  .listen(7777);
+  .listen(7777, () => {
+    console.log("listening at 7777");
+  });
 
 handler.on("error", function(err) {
   console.error("Error:", err.message);
@@ -29,7 +32,7 @@ handler.on("push", function(event) {
     event.payload.repository.name,
     event.payload.ref
   );
-  rumCommand('sh', ['./deploy.sh'], txt => {
-    console.log('deploy====',txt)
-})
+  rumCommand("sh", ["./deploy.sh"], txt => {
+    console.log("deploy====", txt);
+  });
 });
